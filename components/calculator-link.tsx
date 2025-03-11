ba8980cfa8bf83e8a82cd13dd0f817e369ee9179
Jerome Doran <disneyfan9184@gmail.com>
2025-03-11 16:20:44 -0400

    added additional alloy files and a link to alloy search page

---
+++ components/calculator-link.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+
+export default function CalculatorLink() {
+  return (
+    <a
+      href='https://www.advancedalloys.co.uk/alloy-calculator/'
+      target='_blank'
+      rel='noreferrer'
+    >
+      <Button size='lg' className='rounded-full border-2 px-4 py-2'>
+        <span className='text-lg'>Alloy Calculator</span>
+      </Button>
+    </a>
+  );
+}
